f0472ab01f86251a36611e63a6e870a273388d8f
Eric Guyader <ericguyader@yahoo.fr>
2018-07-13 10:42:52 +0200

    Initialise le projet

---
+++ src/components/PrintableCalendars.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="calendars">
+<h2>Impression</h2>
+<h3>
+  Benoît, du 9 au 13 juillet 2018
+</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PrintableCalendars',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Selection.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="selection">
+<h2>Options du calendrier à imprimer</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Selection',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
