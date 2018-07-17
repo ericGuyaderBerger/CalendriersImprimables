80b8a1f58e58d875d11357731b00e9fa4a2a1478
Eric Guyader <ericguyader@yahoo.fr>
2018-07-17 08:58:17 +0200

    Ajoute itération salariés  à la vue des calendriers

--- src/components/PrintableCalendars.vue
+++ src/components/PrintableCalendars.vue
@@ -1,15 +1,21 @@
 <template>
-<div id="calendars">
-<h2>Impression</h2>
-<h3>
-  Benoît, du 9 au 13 juillet 2018
-</h3>
-</div>
+  <div id="calendars">
+    <h2>Impression</h2>
+    <div class="calendar" v-for="salarie in salaries" :key="salarie.nom">
+      <h3>
+        {{nom}}, du 9 au 13 juillet 2018
+      </h3>
+      <table>
+
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'PrintableCalendars',
+  props: salaries
 }
 </script>
 
