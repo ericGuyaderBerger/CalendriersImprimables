02a7077e8c6001579117851b963d2208939d5155
Eric Guyader <ericguyader@yahoo.fr>
2018-07-21 16:23:29 +0200

    Crée composants SelectionSemaine et SelectionSalarie

---
+++ src/components/SelectionSalarie.vue
@@ -0,0 +1,23 @@
+<template>
+  <span :class="{inactive:clicks % 2 > 0}" @click="click">
+    <i class="fa fa-user"></i>
+    {{ salarie }}
+  </span>
+</template>
+<script>
+export default {
+  props:{
+    salarie:String
+  },
+  data(){
+    return {clicks:0}
+  },
+  methods:{
+    click(){
+      this.clicks++
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/components/SelectionSemaine.vue
@@ -0,0 +1,16 @@
+<template>
+  <span :class="{inactive: debut.getTime() !== semaine.debut.getTime() }">
+    <i class="fa fa-calendar"></i> {{ semaine.libelle }}
+  </span>
+</template>
+<script>
+  export default {
+    props:{
+      debut: Date,
+      semaine: Object
+    }
+    // props:['debut','semaine']
+  }
+</script>
+<style scoped>
+</style>
