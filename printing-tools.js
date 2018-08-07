23eb21c46abec88d1d68fe13dd6e43f066b96228
Eric Guyader <ericguyader@yahoo.fr>
2018-08-07 12:28:57 +0200

    Ajoute dialogues et ouverture à impression PDF

--- printing-tools.js
+++ printing-tools.js
@@ -1,20 +1,35 @@
 const fs = require('fs')
-
+const { dialog } = require('electron')
+const opn = require('opn')
 
 module.exports = {
   print(browserWindow){
     browserWindow.webContents.print({landscape:true,printBackground:true})
   },
-  printToPDF(browserWindow){
+  printToPDF(browserWindow, options = null){
+    let fichier = 'c:/users/eric/desktop/rf-ptpdf.pdf'
+    if ( options && options.fichier ) {
+      fichier = options.fichier
+    }
     browserWindow.webContents.printToPDF({landscape:true,printBackground:true}, (err,data) => {
       if(err){
-        
+        dialog.showMessageBox(browserWindow,{
+          type:'error',
+          message:'Impossible de générer le fichier PDF!',
+          detail:err.message
+        })
         return
       }
-      fs.writeFile('c:/users/eric/desktop/rf-ptpdf.pdf', data, (error) => {
+      fs.writeFile(fichier, data, (error) => {
         if (error) throw error
-        // TODO: boîte de dialogue!
-        console.log('Write PDF successfully.')
+        dialog.showMessageBox(browserWindow,{
+          type:'info',
+          message: 'Génération réussie',
+          detail: `Fichier ${fichier} généré correctement!`,
+          buttons: ['OK']
+        })
+
+        opn(fichier)
       })
     })
   }
